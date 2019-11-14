38b4427ecfcd0a7e9ed5ab47bc061c379d5495e5
Darcy <darcy-young@hotmail.com>
2019-11-14 21:15:56 +0000

    Button to reveal cocktail
    Adding the functionality so that when you click the 'gin' button in the carousel, it reveals the gin cocktails

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function reveal_gin() {
+  var x = document.getElementById("gin");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
