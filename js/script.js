10a1a1d6f02c21e68a1f92bb6f6244852ecc6abe
Darcy <darcy-young@hotmail.com>
2019-11-16 19:41:59 +0000

    Fixing the clicking

--- js/script.js
+++ js/script.js
@@ -19,8 +19,44 @@ function reveal_gin() {
   }
 }
 
-function cancel_vodka() {
-    // Removes an element from the document
-    var element = document.getElementById("vodka");
-    element.parentNode.removeChild("vodka");
+function reveal_rum() {
+  var x = document.getElementById("rum");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
+
+$(document).mouseup(function (e)
+                    {
+  var container = $("#gin"); // YOUR CONTAINER SELECTOR
+
+  if (!container.is(e.target) // if the target of the click isn't the container...
+      && container.has(e.target).length === 0) // ... nor a descendant of the container
+  {
+    container.hide();
+  }
+});
+
+$(document).mouseup(function (e)
+                    {
+  var container = $("#vodka"); // YOUR CONTAINER SELECTOR
+
+  if (!container.is(e.target) // if the target of the click isn't the container...
+      && container.has(e.target).length === 0) // ... nor a descendant of the container
+  {
+    container.hide();
+  }
+});
+
+$(document).mouseup(function (e)
+                    {
+  var container = $("#rum"); // YOUR CONTAINER SELECTOR
+
+  if (!container.is(e.target) // if the target of the click isn't the container...
+      && container.has(e.target).length === 0) // ... nor a descendant of the container
+  {
+    container.hide();
+  }
+});
